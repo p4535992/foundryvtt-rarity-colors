4fa1ce981dfdaa6174f2056932cd446dd5bfbac5
p4535992 <tentimarco0@gmail.com>
2023-03-08 14:06:42 +0100

    synced with lonely bug bear

--- scripts/raritycolors.js
+++ scripts/raritycolors.js
@@ -4,7 +4,7 @@ Hooks.on('renderActorSheet', (actor, html) => {
     for(let i of items) {
         let id = i.outerHTML.match(/data-item-id="(.*?)"/)
         if(!id) return
-        let rarity = actor.object.items.get(id[1]).system.rarity
+        let rarity = actor.object.items.get(id[1]).getRollData()?.item.rarity
         if(rarity !== "" && rarity !== undefined) i.classList.add(rarity.slugify().toLowerCase())
     }
 });
@@ -18,8 +18,8 @@ Hooks.on("renderSidebarTab", (bar, html) => {
         let id = i.outerHTML.match(/data-document-id="(.*?)"/)
         if(!id) return
         let item = game.items.get(id[1])
-        let rarity = item.system.rarity
-        let type = item.type
+        let rarity = item?.system?.rarity
+        let type = item?.type
         if(rarity !== "" && rarity !== undefined && rarityFlag) i.classList.add(rarity.slugify().toLowerCase())
         if(type === "spell" && spellFlag) i.classList.add("spell")
         if(type === "feat" && featFlag) i.classList.add("feat")
